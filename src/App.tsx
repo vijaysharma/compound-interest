157e1f1935b3f2a963057dcd0595454a831179f2
Vijay Sharma <Vijay.Sharma@causeway.com>
2024-11-06 19:29:57 +0530

    Fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,4 @@ import Home from "./pages/home";
 function App() {
   return <Home />;
 }
-
-const a = [{"schemeCode":105578,"schemeName":"UTI - Treasury Advantage Fund-  Discontinued - Periodic Dividend Plan"},{"schemeCode":105578,"schemeName":"UTI - Treasury Advantage Fund - Discontinued - Periodic Dividend Plan"},{"schemeCode":105578,"schemeName":"UTI Low Duration Fund - Discontinued - Periodic Dividend Plan"}]
 export default App;
